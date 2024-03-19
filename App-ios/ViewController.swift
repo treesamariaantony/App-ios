c3e78f00274422392862a11780503eb80073fba6
Treesa <tresatritto28@gmail.com>
2024-03-19 20:38:10 +0100

    push second pqrrt of view controller

--- App-ios/ViewController.swift
+++ App-ios/ViewController.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let viewModel: LoginViewModelProtocol
+    
+    required init?(coder aDecoder: NSCoder) {
+        self.viewModel = LoginViewModel()
+        super.init(coder: aDecoder)
+    }
+    
     @IBOutlet weak var errorLabel: UILabel!
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var passwordTextfield: UITextField!
@@ -16,16 +24,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var loginImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
-       
+        
         // Do any additional setup after loading the view.
     }
-
-    func setupUI(){
     
+    func setupUI(){
+        
         titleLabel.text = "Welcome!"
         titleLabel.font = UIFont(name: "Helvetica-Bold", size: 24.0)
         loginImageView.image = UIImage(named : "Mcd")
@@ -44,9 +52,19 @@ class ViewController: UIViewController {
         errorLabel.isHidden = true
         
     }
-
-    @IBAction func buttonClicked(_ sender: UIButton) {
-    }
-}
-
     
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        let canLogin = viewModel.login(username: usernamTextField.text, password: passwordTextfield.text)
+        if canLogin{
+            errorLabel.isHidden = canLogin
+            let myViewController = SecondViewController()
+            self.navigationController?.pushViewController(myViewController, animated: true)
+        }
+        else {
+            errorLabel.isHidden = canLogin
+        }
+        
+    }
+    
+    
+}
--- App-ios/ViewModels/LoginViewModel.swift
+++ App-ios/ViewModels/LoginViewModel.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+protocol LoginViewModelProtocol{
+    func login(username:String?, password : String?) -> Bool
+//func resolveSecondViewModel() -> SecondScreenViewModelProtocol
+}
+
+class LoginViewModel : LoginViewModelProtocol{
+    
+    func login(username: String?, password: String?) -> Bool {
+        guard let username = username else { return false}
+        guard let password = password else { return false}
+        
+        if username == "treesa" && password == "1234"{
+            return true
+        }
+        return false
+    }
+    
+//    func resolveSecondViewModel() -> SecondScreenViewModelProtocol {
+//        let viewModel = SecondScreenViewModel()
+//        return viewModel
+//    }
+}
